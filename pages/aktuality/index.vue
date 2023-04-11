b06245ecf7bfee78f3786ecb0fbd4dfbcee88dc8
David Krejci <taavetti.krejci@seznam.cz>
2023-04-11 11:50:57 +0200

    redid layout

--- pages/aktuality/index.vue
+++ pages/aktuality/index.vue
@@ -4,15 +4,29 @@
       <h1 class="text-2xl font-semibold mb-6">Aktuality</h1>
       <section class="space-y-4 divide-y">
         <article v-for="post of posts" :key="post.slug" class="pt-4">
-          <h2 class="text-lg mb-1 font-semibold text-orange-400">
-            <nuxt-link :to="`/aktuality/${post.slug}`">
-              {{ post.title }}
-            </nuxt-link>
-          </h2>
-          <div class="mb-2 text-sm text-neutral-300">{{ formatDate(post.date) }}</div>
-          <span>
-            {{ post.description }}
-          </span>
+          <div class="lg:flex">
+                <img class="object-cover w-full h-56 rounded-lg lg:w-64" :src="'/images/articles/' + post.slug + '.jpg'" alt="">
+
+                <div class="flex flex-col pt-4 lg:mx-6">
+                    <div class="mb-2 text-sm text-neutral-400">{{ formatDate(post.date) }}</div>
+                    <div>
+                      <h2 class="text-xl mb-4 font-semibold hover:text-orange-400">
+                        <nuxt-link :to="`/aktuality/${post.slug}`">
+                          {{ post.title }}
+                        </nuxt-link>
+                      </h2>
+                      <span>
+                        {{ post.description }}
+                      </span>
+                      <nuxt-link class="text-orange-400" :to="`/aktuality/${post.slug}`">
+                          Číst dále...
+                        </nuxt-link>
+                    </div>
+                  </div>
+            </div>
+
+          
+          
         </article>
       </section>
     </div>
