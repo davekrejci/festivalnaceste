fc532dd40560e6a0c0fa722473272b1d10fc4302
David Krejci <taavetti.krejci@seznam.cz>
2023-04-11 11:44:45 +0200

    copyright edit

--- components/customFooter.vue
+++ components/customFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer items-center p-4 text-neutral-400 bg-base-300">
       <div class="items-center grid-flow-col">
-        <p class="text-xs">Copyright © Festival Na Cestě 2023 - All rights reserved</p>
+        <p class="text-xs">Copyright © Festival Na Cestě 2023</p>
       </div> 
   </footer>
   </template>
