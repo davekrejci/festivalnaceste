6b067096cde2e0454495fb9887baa3193665d6e7
David Krejci <taavetti.krejci@seznam.cz>
2023-04-11 11:51:28 +0200

    started on media page

--- pages/pro-media.vue
+++ pages/pro-media.vue
@@ -1,5 +1,60 @@
 <template>
-    <section>
-      <p>This page will be displayed at the /pro-media route.</p>
+  <div class="p-8 md:px-16 lg:w-3/5 mx-auto text-neutral-300 divide-y">
+    <section class="py-8 pt-4">
+      <h1 class="font-bold text-3xl mb-8">Pro média</h1>
+      <h2 class="font-bold text-2xl mb-2">
+        Kontakty
+      </h2>
+      <div class="flex flex-col items-left">
+        <span class="font-bold text-lg">Lucie Tománkova</span>
+        <span>Vedoucí mediální komunikace a PR</span>
+        <span>+420 730 959 633</span>
+        <span>tom0225@slu.cz</span>
+      </div>
     </section>
+    <!-- <section class="py-8">
+      <h2 class="font-bold text-2xl mb-2">
+        Tiskové zprávy
+      </h2>
+      <div class="">
+        <div class="flex py-2">
+          <div class="flex-grow">
+            <span>21.3.2023</span>
+            <p>Tisková zpráva č.1</p>
+          </div>
+          <div class="my-auto">
+            <a href="#" class="bg-orange-600 text-white p-2 px-4 rounded-md">Stáhnout soubor</a>
+          </div>
+        </div>
+        <div class="flex py-2">
+          <div class="flex-grow">
+            <span>21.3.2023</span>
+            <p>Tisková zpráva č.2</p>
+          </div>
+          <div class="my-auto">
+            <a href="#" class="bg-orange-600 text-white p-2 px-4 rounded-md">Stáhnout soubor</a>
+          </div>
+        </div>
+        
+      </div>
+    </section> -->
+    <section class="py-8">
+      <h2 class="font-bold text-2xl mb-4">
+        Materiály pro média
+      </h2>
+
+      <div class="grid grid-cols-1 xl:grid-cols-3 gap-8">
+        <div class="card bg-[#341819] p-8 pt-4 min-h-64 shadow-xl flex ">
+          <div>
+            <img class="h-16 w-auto" src="@/assets//images/logo.svg" alt="" />
+            <span class="font-bold mb-4">Loga ke stažení</span>
+          </div>
+          <span class="text-neutral-300 mb-6">Zazipovaná složka s několika formáty našeho loga</span>
+          <div>
+            <a href="#" class="bg-orange-600 text-white p-2 px-4 rounded-md">Stáhnout soubor</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
   </template>
