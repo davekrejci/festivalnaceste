b1bf1f6ebca557beddb436f9a15e410255ab7906
David Krejci <taavetti.krejci@seznam.cz>
2023-08-04 13:13:37 +0200

    jumping button

---
+++ components/jumpingButton.vue
@@ -0,0 +1,62 @@
+<template>
+    <a @click="scrollToSection" class="jumpingButton bg-orange-600 border-orange-400 border-4 rounded-full text-center">
+        <Icon name="formkit:caretdown" class="text-neutral-200 mb-2 font-bold" size="16"></Icon>
+    </a>
+</template>
+
+<script setup>
+const props = defineProps({
+    to: {
+        type: String,
+    }
+})
+
+function scrollToSection() {
+    console.log(props.to);
+    const section = document.getElementById(props.to);
+    section.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest', duration: 10000 });
+}
+    
+</script>
+
+<style>
+.jumpingButton {
+    -webkit-animation: jumpingScroll 1s infinite;
+    animation: jumpingScroll 1s infinite;
+    cursor: pointer;
+    display: block;
+    height: 28px;
+    margin: 0 auto;
+    position: relative;
+    top: 0;
+    width: 28px;
+}
+
+@-webkit-keyframes jumpingScroll {
+    0% {
+        margin-top: 3px
+    }
+
+    50% {
+        margin-top: -10px
+    }
+
+    100% {
+        margin-top: 3px
+    }
+}
+
+@keyframes jumpingScroll {
+    0% {
+        margin-top: 3px;
+    }
+
+    50% {
+        margin-top: -10px;
+    }
+
+    100% {
+        margin-top: 3px;
+    }
+}
+</style>
