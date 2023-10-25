1d64f15aba07986ca3d718fc2f53800e10ba1b9d
davekrejci <taavetti.krejci@seznam.cz>
2023-10-25 23:32:04 +0200

    logo image display fix

--- components/sponzori.vue
+++ components/sponzori.vue
@@ -1,10 +1,10 @@
 <template>
     <h2 class="text-xl text-base-100 font-bold leading-7 sm:truncate sm:text-2xl sm:tracking-tight text-center text-white">Hlavní partneři</h2>
     <div class="grid grid-cols-2 md:grid-cols-4 gap-x-8 gap-y-16">
-        <div class="max-w-[150px]"><NuxtImg  fit="cover" height="50" format="webp" src="/images/logos/dilia-white.svg" alt="logo-dilia"/></div>
-        <div class="max-w-[150px]"><NuxtImg  fit="cover" height="50" format="webp" src="/images/logos/slu-white.svg" alt="logo-slu"/></div>
-        <div class="max-w-[150px]"><NuxtImg  fit="cover" height="50" format="webp" src="/images/logos/oko-white.svg" alt="logo-oko"/></div>
-        <div class="max-w-[150px]"><NuxtImg  fit="cover" height="50" format="webp" src="/images/logos/logo_opava.svg" alt="logo-opava"/></div>
+        <div class="max-w-[150px]"><NuxtImg  fit="cover" height="50" class="w-full" format="webp" src="/images/logos/dilia-white.svg" alt="logo-dilia"/></div>
+        <div class="max-w-[150px]"><NuxtImg  fit="cover" height="50" class="w-full" format="webp" src="/images/logos/slu-white.svg" alt="logo-slu"/></div>
+        <div class="max-w-[150px]"><NuxtImg  fit="cover" height="50" class="w-full" format="webp" src="/images/logos/oko-white.svg" alt="logo-oko"/></div>
+        <div class="max-w-[150px]"><NuxtImg  fit="cover" height="50" class="w-full" format="webp" src="/images/logos/logo_opava.svg" alt="logo-opava"/></div>
     </div>
 </template>
     
